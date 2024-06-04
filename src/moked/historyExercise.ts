2b91803d0bf7410466a471f506acb5fa74ac14f5
Sh4rk-dev <renanrapace13@gmail.com>
2024-06-04 13:27:45 -0300

    Create component: HistoryCard

---
+++ src/moked/historyExercise.ts
@@ -0,0 +1,12 @@
+export const Data = [
+  {
+    id: "1",
+    title: "26.08.2022",
+    data: ["Puxada Frontal", "Remada Unilateral"]
+  },
+  {
+    id: "2",
+    title: "27.08.2022",
+    data: ["Puxada Lateral"]
+  }
+];
