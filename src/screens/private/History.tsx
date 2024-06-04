01b337b651aa6fb954b4a37bcb3adc927760fa69
Sh4rk-dev <renanrapace13@gmail.com>
2024-06-04 13:30:09 -0300

    Implements the SectionList in the history screen

--- src/screens/private/History.tsx
+++ src/screens/private/History.tsx
@@ -1,12 +1,44 @@
+import { useState } from "react";
+
+import { SectionList } from "react-native";
+import { Heading, VStack } from "native-base";
+
+import { Data } from "@moked/historyExercise";
+
 import { Header } from "@components/Header";
-import { VStack } from "native-base";
+import { HistoryCard } from "@components/HistoryCard";
 
 export function History() {
+  const HistoryExercise = Data;
+
   return (
     <VStack flex={1}>
       <Header>
         <Header.Title position={"center"} title="Histórico de Exercícios" />
       </Header>
+
+      <SectionList
+        contentContainerStyle={{
+          paddingHorizontal: 20
+        }}
+        sections={HistoryExercise}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <HistoryCard />}
+        renderSectionHeader={({ section }) => {
+          return (
+            <Heading
+              mt={10}
+              mb={5}
+              mx={5}
+              fontSize={"md"}
+              color={"gray.100"}
+              textTransform={"capitalize"}
+            >
+              {section.title}
+            </Heading>
+          );
+        }}
+      />
     </VStack>
   );
 }
