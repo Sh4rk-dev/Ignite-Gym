b37a62d5365ca1616fc2d778c25591f56a738bd6
Sh4rk-dev <renanrapace13@gmail.com>
2024-06-06 17:23:11 -0300

    Add the title to the header on the assignment details screen

--- src/screens/private/Exercise.tsx
+++ src/screens/private/Exercise.tsx
@@ -1,10 +1,10 @@
-import { Icon, VStack } from "native-base";
 import { TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { HStack, Heading, Icon, Text, VStack } from "native-base";
 
-import { Header } from "@components/Header";
+import BodySVG from "@assets/body.svg";
 
 import { Feather } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
 import { AppNavigationTabsRoutesProps } from "@routes/app.routes";
 
 export function Exercise() {
@@ -16,11 +16,30 @@ export function Exercise() {
 
   return (
     <VStack flex={1}>
-      <Header>
+      <VStack px={8} pt={12} bg={"gray.600"}>
         <TouchableOpacity onPress={handleGoBack}>
           <Icon size={6} as={Feather} name="arrow-left" color={"green.500"} />
         </TouchableOpacity>
-      </Header>
+
+        <HStack
+          mt={4}
+          mb={8}
+          alignContent={"center"}
+          justifyContent={"space-between"}
+        >
+          <Heading color={"gray.100"} fontSize={"lg"} flexShrink={1}>
+            Puxada frontal
+          </Heading>
+
+          <HStack alignItems={"center"}>
+            <BodySVG />
+
+            <Text color={"gray.200"} ml={1} textTransform={"capitalize"}>
+              Costas
+            </Text>
+          </HStack>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
