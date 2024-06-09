1420db689d6007b42a86497d9177636f1de0bb45
Renan <renanrapace13@gmail.com>
2024-06-09 11:25:36 -0300

    Update: Style the input according to the error

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,22 +1,43 @@
-import { Input as NativeBaseInput, IInputProps } from "native-base";
+import { Input as NativeBaseInput, IInputProps, Text } from "native-base";
 
-export function Input({ ...rest }: IInputProps) {
+interface IInputTextProps extends IInputProps {
+  label?: string;
+}
+
+export function Input({ label, ...rest }: IInputTextProps) {
   return (
-    <NativeBaseInput
-      h={14}
-      px={4}
-      mb={4}
-      borderWidth={0}
-      fontSize={"md"}
-      color={"white"}
-      bg={"gray.700"}
-      _focus={{
-        bg: "gray.700",
-        borderLeftWidth: 2,
-        borderColor: "green.500",
-      }}
-      placeholderTextColor="gray.300"
-      {...rest}
-    />
+    <>
+      {label && (
+        <Text color="red.500" flex={1} w={"full"}>
+          {label}
+        </Text>
+      )}
+
+      <NativeBaseInput
+        h={14}
+        px={4}
+        mb={4}
+        borderWidth={label ? 1 : 0}
+        borderColor={label && "red.500"}
+        fontSize={"md"}
+        color={"white"}
+        bg={"gray.700"}
+        _focus={
+          label
+            ? {
+                bg: "gray.700",
+                borderWidth: 1,
+                borderColor: "red.500",
+              }
+            : {
+                bg: "gray.700",
+                borderLeftWidth: 2,
+                borderColor: "green.500",
+              }
+        }
+        placeholderTextColor={label ? "red.500" : "gray.300"}
+        {...rest}
+      />
+    </>
   );
 }
