d1e8e338197d7249af91d32e8a468d15b0adccf3
Renan <renanrapace13@gmail.com>
2024-06-13 13:37:17 -0300

    Create the context and global state of the application

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,43 @@
+import { ReactNode, createContext, useState } from "react";
+
+import { UserDTO } from "@dtos/UserDTO";
+import { api } from "@services/api";
+
+export interface AuthContextDataProps {
+  user: UserDTO;
+  signIn: (email: string, password: string) => Promise<void>;
+}
+
+interface IAuthContextProviderProps {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext<AuthContextDataProps>(
+  {} as AuthContextDataProps
+);
+
+export function AuthContextProvider({ children }: IAuthContextProviderProps) {
+  const [user, setUser] = useState<UserDTO>({} as UserDTO);
+
+  async function signIn(email: string, password: string) {
+    try {
+      const { data } = await api.post("/sessions", { email, password });
+      if (data.user) {
+        setUser(data.user);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        signIn,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
